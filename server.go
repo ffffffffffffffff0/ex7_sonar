9faf1b47d0f595b55424fbf688f0914bdbe0e291
ffffffffffffffff0 <grtmt@interia.pl>
2024-05-13 11:32:02 +0200

    Constant instead of literal in routes

--- server.go
+++ server.go
@@ -11,23 +11,25 @@ import (
 func main() {
 	database.ConnectDB()
 	e := echo.New()
-	
+	p := "/products/:id"
+	c := "/categories/:id"
+	b := "/basket/:id"
 
-	e.GET("/products/:id", products.GetProduct, products.CheckIdMiddleware)
-	e.PUT("/products/:id", products.UpdateProduct, products.CheckIdMiddleware)
-	e.DELETE("/products/:id", products.DeleteProduct, products.CheckIdMiddleware)
+	e.GET(p, products.GetProduct, products.CheckIdMiddleware)
+	e.PUT(p, products.UpdateProduct, products.CheckIdMiddleware)
+	e.DELETE(p, products.DeleteProduct, products.CheckIdMiddleware)
 	e.GET("/products", products.GetProducts)
 	e.POST("/products", products.CreateProduct)
 
-	e.GET("/categories/:id", categories.GetCategory, categories.CheckIdMiddleware)
-	e.PUT("/categories/:id", categories.UpdateCategory, categories.CheckIdMiddleware)
-	e.DELETE("/categories/:id", categories.DeleteCategory, categories.CheckIdMiddleware)
+	e.GET(c, categories.GetCategory, categories.CheckIdMiddleware)
+	e.PUT(c, categories.UpdateCategory, categories.CheckIdMiddleware)
+	e.DELETE(c, categories.DeleteCategory, categories.CheckIdMiddleware)
 	e.GET("/categories", categories.GetCategories)
 	e.POST("/categories", categories.CreateCategory)
 
-	e.GET("/basket/:id", basket.GetBasketItem, basket.CheckIdMiddleware)
-	e.PUT("/basket/:id", basket.UpdateBasketItem, basket.CheckIdMiddleware)
-	e.DELETE("/basket/:id", basket.DeleteBasketItem, basket.CheckIdMiddleware)
+	e.GET(b, basket.GetBasketItem, basket.CheckIdMiddleware)
+	e.PUT(b, basket.UpdateBasketItem, basket.CheckIdMiddleware)
+	e.DELETE(b, basket.DeleteBasketItem, basket.CheckIdMiddleware)
 	e.GET("/basket", basket.GetBasket)
 	e.POST("/basket", basket.AddToBasket)
 
